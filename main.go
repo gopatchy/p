1b1a191551b10b99b1138861d9c0676a906dcb52
Ian Gulliver <github@flamingcow.io>
2024-11-21 19:30:11 -0800

    Static path

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello! you've requested %s\n", r.URL.Path)
-	})
+//	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+//		fmt.Fprintf(w, "Hello! you've requested %s\n", r.URL.Path)
+//	})
 
-	http.Handle("/static", http.FileServer(http.Dir("./static")))
+	http.Handle("/", http.FileServer(http.Dir("./static")))
 
 	port := os.Getenv("PORT")
 	if port == "" {
